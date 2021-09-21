dca5db571a722398ded7fa46f843a43984d55394
Omkar <omkar.betha@gmail.com>
2021-09-21 13:05:54 +0530

    ss

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const  createProxyMiddleware  = require('http-proxy-middleware');
+const url = process.env.CONTAINER_NAME
+module.exports = function(app) {
+  app.use(
+    createProxyMiddleware({
+      target: url,
+      changeOrigin: true,
+    })
+  );
+};
