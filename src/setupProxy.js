6d638ab9a56ed71fab7f93482bf5e2eb63b4f298
Omkar <omkar.betha@gmail.com>
2021-09-22 19:58:05 +0530

    ss

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,5 +12,5 @@ const proxy = require('http-proxy-middleware');
 //   );
 // };
 module.exports = function(app) {
-  app.use(proxy('/api/customers', { target: url }))
+  app.use(proxy('/api/customer', { target: url }))
 }
